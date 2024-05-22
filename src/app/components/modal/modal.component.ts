27913528e2a23f67dae346fbad1641f4cf7b13bc
22051988Di <vitalii.soloninko@gmail.com>
2024-05-22 16:15:16 +0200

    v0.0.26	create-product form component

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.scss'
+  styleUrl: './modal.component.scss',
 })
-export class ModalComponent {
-
+export class ModalComponent implements OnInit {
+  @Input() title: string;
+  ngOnInit(): void {}
 }
