03a6e54ad9a09f9ad9ecd83ffe4d6d16d34b429e
22051988Di <vitalii.soloninko@gmail.com>
2024-05-22 16:34:24 +0200

    v0.0.27	work with form, error

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { title } from 'process';
 
 @Component({
   selector: 'app-create-product',
   templateUrl: './create-product.component.html',
-  styleUrl: './create-product.component.scss'
+  styleUrl: './create-product.component.scss',
 })
-export class CreateProductComponent {
+export class CreateProductComponent implements OnInit {
+  form = new FormGroup({
+    title: new FormControl<string>(''),
+  });
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
